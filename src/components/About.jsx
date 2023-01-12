f8bfa69f93fec8f4b1ad192cce43c8b0ba02262d
Dev Julian <dev.achmadjulian@gmail.com>
2023-01-12 09:30:48 +0700

    fix sidebar and modified about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,9 +16,9 @@ export default function About() {
 
             <section className={`about-container`}>
 
-                <p className={`about about-${theme}`}>Usually called Julian, I am a semester 2 student majoring in Engineering Informatics at Unpam, even though I'm still in semester 2, I really want to being a Frontend Developer, therefore I am self-taught and want to do an internship to apply the knowledge I have learned.</p>
+                <p className={`about about-${theme}`}>Usually called Julian, I am a semester 2 student majoring in Engineering Informatics at Universitas Pamulang, even though I'm still in semester 2, I really want to being a Frontend Developer, therefore I am self-taught and want to do an internship to apply the knowledge I have learned.</p>
 
-                <p className={`about about-${theme}`}>Biasa dipanggil Julian, saya mahasiswa semester 2 jurusan Teknik Informatika di Unpam, walaupun saya masih semester 2, saya ingin sekali menjadi Frontend Developer, maka dari itu saya belajar secara otodidak dan ingin magang untuk mengaplikasikan ilmu yang sudah saya pelajari.</p>
+                <p className={`about about-${theme}`}>Biasa dipanggil Julian, saya mahasiswa semester 2 jurusan Teknik Informatika di Universitas Pamulang, walaupun saya masih semester 2, saya ingin sekali menjadi Frontend Developer, maka dari itu saya belajar secara otodidak dan ingin magang untuk mengaplikasikan ilmu yang sudah saya pelajari.</p>
             </section>
         </main>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,17 @@
 import ThemeButton from "./ThemeButton";
 
 import { useContext } from "react";
-import { currentTheme, sidebarOn } from "../Context.mjs";
+import { currentTheme } from "../Context.mjs";
 import Sidebar from "./Sidebar";
 import MenuButton from "./MenuButton";
 import Links from "./Links";
 
 export default function Navbar() {
     const [theme] = useContext(currentTheme);
-    const [sidebar] = useContext(sidebarOn);
-
-    let showSidebar = sidebar === "open" && <Sidebar />;
 
     return (
         <header className={`navbar navbar-${theme}`}>
-            {showSidebar}
+            <Sidebar />
             <MenuButton />
             <Links />
             <ThemeButton />
